fecc60fefdcd6f2fbbf0c22033d463d2f6ead1c3
isadoratavare <isadoratavare85@gmail.com>
2023-09-12 23:48:42 -0300

    Search Bar in home screen

--- EcommerceApp/ContentView.swift
+++ EcommerceApp/ContentView.swift
@@ -9,7 +9,7 @@ struct ContentView: View {
             VStack{
                 AppBarView()
                 
-                Text("Search").padding()
+                SearchBar()
                 
                 Text("Carrousel").padding()
                 
@@ -74,5 +74,38 @@ struct AppBarView: View {
             }
         }
         .padding(.horizontal)
+    }
+}
+
+struct SearchBar: View {
+    @State private var search: String = "";
+    var body: some View {
+        HStack{
+            HStack{
+                TextField("Search", text: $search)
+                Button(action: {
+                    if search != "" {
+                        self.search = ""
+                    }
+                }) {
+                    Image(systemName: search == "" ? "magnifyingglass" : "xmark")
+                        .padding(.trailing, 8)
+                        .foregroundColor(Color.black)
+                }
+            }
+            .padding()
+            .background(Color.white)
+            .cornerRadius(10.0)
+            
+            Button(action: {}) {
+                Image(systemName: "slider.horizontal.3")
+                    .font(.system(size: 20))
+                    .padding(10.0)
+                    .background(Color("Salmon"))
+                    .foregroundColor(Color.white)
+                    .cornerRadius(10.0)
+                
+            }
+        }.padding(.horizontal)
     }
 }
