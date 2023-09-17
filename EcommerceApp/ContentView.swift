288638789adaeb32cdef94ed661bbb178619a74a
isadoratavare <isadoratavare85@gmail.com>
2023-09-17 20:00:36 -0300

    Finishing visual home and organizing

--- EcommerceApp/ContentView.swift
+++ EcommerceApp/ContentView.swift
@@ -1,36 +1,16 @@
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        ZStack{
-            Color("background")
-                .edgesIgnoringSafeArea(.all)
-            
-            VStack{
-                AppBar()
-                
-                SearchBar()
-                
-                ScrollView {
-                    Banner()
-                    
-                    Categories()
-                    
-                    ProductList()
-                }
-                
-                Spacer()
-                Text("Bottom Menu")
-            }
-        }
-    }
-}
+    var body: some View{
+        HomeView()
+    }}
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
     }
 }
+
 
 
 
